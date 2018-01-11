47f9457356e3a263afe3434f9f446c48da540e6e
Joseph Tomlinson <bizzclaw1@gmail.com>
2018-01-10 23:44:20 -0800

    add GetVersion method to C++ for lua checks

--- src/gmcl_gvr.cpp
+++ src/gmcl_gvr.cpp
@@ -4,22 +4,32 @@
 #include "GarrysMod/Lua/Interface.h"
 #include "openvr.h"
 
+int version = 1; //every release this will be incremented, in lua, the user will be warned to update the dll if they have a lua version ahead of the module.
+
 using namespace GarrysMod::Lua;
 using namespace vr;
 
 IVRSystem *m_pHMD;
 HmdMatrix34_t HmdMatrix;
 
+LUA_FUNCTION( GetVersion )
+{
+	LUA->PushNumber(version);
+	return 1;
+}
+
 LUA_FUNCTION( IsHmdPresent )
 {
 		LUA->PushBool(VR_IsHmdPresent());
 		return 1;
 }
+
 GMOD_MODULE_OPEN()
 {
     LUA->PushSpecial( SPECIAL_GLOB );
     LUA->PushString( "gmodvr" );
-    LUA->PushCFunction( IsHmdPresent );
+	LUA->PushCFunction( GetVersion );
+	LUA->PushCFunction( IsHmdPresent );
     LUA->SetTable( -3 );
 
     return 0;
